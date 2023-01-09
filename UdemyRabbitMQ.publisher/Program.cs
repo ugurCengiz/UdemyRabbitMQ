7c805d872ddc0f943c3e8251a7b70b064abb6ac9
uğur cengiz <ugur_nobre@hotmail.com>
2023-01-09 17:18:59 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Publisher
+using System.Net.Http.Headers;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://ohictzrn:SEm-Y_sHOYw0AQM7WxkF7zecjptRF2bi@cow.rmq2.cloudamqp.com/ohictzrn");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(1,50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+    Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+});
+
+
+
+Console.ReadLine();
+
+
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Subscriber
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://ohictzrn:SEm-Y_sHOYw0AQM7WxkF7zecjptRF2bi@cow.rmq2.cloudamqp.com/ohictzrn");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// channel.QueueDeclare("hello-queue", true, false, false);
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue",false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1500);
+    Console.WriteLine("Gelen Mesaj:" + message);
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+
+
+Console.ReadLine();
