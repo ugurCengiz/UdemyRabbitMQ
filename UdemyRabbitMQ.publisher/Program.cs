faa7b7bad8c9602d0cfa412f8a84ea74c30f0d45
uğur cengiz <ugur_nobre@hotmail.com>
2023-01-10 16:39:21 +0300

    10.01.23

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -2,30 +2,53 @@
 //Publisher
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using RabbitMQ.Client;
+using Shared;
 
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://ohictzrn:SEm-Y_sHOYw0AQM7WxkF7zecjptRF2bi@cow.rmq2.cloudamqp.com/ohictzrn");
 
-using var connection = factory.CreateConnection();
-
-var channel = connection.CreateModel();
-
-channel.QueueDeclare("hello-queue", true, false, false);
-
-Enumerable.Range(1,50).ToList().ForEach(x =>
+public enum LogNames
 {
-    string message = $"Message {x}";
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
 
-    var messageBody = Encoding.UTF8.GetBytes(message);
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://ohictzrn:SEm-Y_sHOYw0AQM7WxkF7zecjptRF2bi@cow.rmq2.cloudamqp.com/ohictzrn");
 
-    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+        using var connection = factory.CreateConnection();
 
-    Console.WriteLine($"Mesaj gönderilmiştir : {message}");
-});
+        var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+        Dictionary<string, object> headers = new Dictionary<string, object>();
+
+        headers.Add("format","pdf");
+        headers.Add("shape","a4");
+
+        var properties = channel.CreateBasicProperties();
+        properties.Headers = headers;
+        properties.Persistent = true;
+
+        var product = new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 10 };
+        var productJsonString = JsonSerializer.Serialize(product);
+        
+        channel.BasicPublish("header-exchange",string.Empty,properties,Encoding.UTF8.GetBytes(productJsonString));
+
+        Console.WriteLine("mesaj gönderilmiştir.");
+        Console.ReadLine();
+    }
 
 
+}
 
-Console.ReadLine();
+
 
 
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -2,7 +2,9 @@
 //Subscriber
 using RabbitMQ.Client;
 using System.Text;
+using System.Text.Json;
 using RabbitMQ.Client.Events;
+using Shared;
 
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://ohictzrn:SEm-Y_sHOYw0AQM7WxkF7zecjptRF2bi@cow.rmq2.cloudamqp.com/ohictzrn");
@@ -11,19 +13,37 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-// channel.QueueDeclare("hello-queue", true, false, false);
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
 
 channel.BasicQos(0,1,false);
 
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume("hello-queue",false,consumer);
+var queueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+headers.Add("x-mac","all");
+
+channel.QueueBind(queueName,"header-exchange",String.Empty,headers);
+
+channel.BasicConsume(queueName,false,consumer);
+
+Console.WriteLine("Loglar dinleniyor...");
 
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
-    Thread.Sleep(1500);
-    Console.WriteLine("Gelen Mesaj:" + message);
+
+    Product product = JsonSerializer.Deserialize<Product>(message);
+
+    Thread.Sleep(1000);
+    Console.WriteLine($"Gelen Mesaj:{product.Id}-{product.Name}-{product.Price}-{product.Stock}");
+
+   
+
     channel.BasicAck(e.DeliveryTag,false);
 };
 
